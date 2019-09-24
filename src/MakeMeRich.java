af0c588b1801bd92586d3d4d8d2fb6cac96b9873
Aca <Aca@DESKTOP-BJ8GGNH>
2019-09-24 15:50:24 +0200

    jD

--- src/MakeMeRich.java
+++ src/MakeMeRich.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,13 +12,17 @@ public class MakeMeRich {
 	public static void main(String[] args) {
 
 		// 1. Print these symbols using a Java 8 for-each and lambdas
-
+		symbols.stream().forEach((symbol1) -> System.out.print(symbol1 + ", "));
 		// 2. Use the StockUtil class to print the price of Bitcoin
-
+		symbols.stream().forEach(symbol1 -> System.out.println(StockUtil.getPrice(symbol1)));
 		// 3. Create a new List of StockInfo that includes the stock price
-	
-		// 4. Find the highest-priced stock under $500
-	
+		List<StockInfo> list = new ArrayList<StockInfo>();
+		for (String string : symbols) {
+			list.add(StockUtil.getPrice(string));}
+		System.out.println(list);
+	// 4. Find the highest-priced stock under $500
+	StockInfo HP = list.stream().filter(StockUtil.isPriceLessThan(500)).reduce(StockUtil::pickHigh).get();
+	System.out.println("The highest priced stock under 500 dollars is " +HP);
 	}
 
 }
